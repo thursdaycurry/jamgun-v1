9837c316bd2dd342bacd4546177fcb23cad01e9c
thursdaycurry <thursdaycurry@gmail.com>
2023-03-29 23:41:21 +0900

    FEATURE one scroll unit scraping feature

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import os
 # Load env vars from .env
 from dotenv import load_dotenv
 load_dotenv()
+from datetime import date
 
 # Set up driver
 web_intelliPick = 'https://intellipick.spartacodingclub.kr/'
@@ -63,18 +64,13 @@ print('🦎 Success: final login button clicked')
 
 # 🦉 Listening Stage ----------------------------------------
 
-# Create List
-
-
-company_data = []
-job_title_data = []
-job_contract_data = []
-location_data = []
-
 # Infinite Scroll until touch down to the bottom
 last_height = driver.execute_script("return document.body.scrollHeight")
 scrolling = True
-while scrolling:
+
+# while scrolling:
+
+for x in range(2):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     print('🦉 Moved to next scroll')
     time.sleep(3)
@@ -86,9 +82,91 @@ while scrolling:
     else:
         last_height = new_height
 
-# Get all urls
-# url_resources = driver.find_elements_by_xpath("//a[contains(@href, '/companies-to-app')]")
+# Get url resources and convert into url strings
 url_resources = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//a[contains(@href, '/companies-to-app')]")))
+url_list = [url.get_attribute('href') for url in url_resources]
+print('🦉 All urls are extracted')
+print(f'url_list len : {len(url_list)}')
 
-for url in url_resources:
-    print(url.get_attribute("href"))
+# Initialize data list
+company_data = []
+job_title_data = []
+job_contract_data = []
+location_data = []
+application_data = []
+appliedToday_data = []
+
+for url in url_list:
+
+    driver.get(url)
+    profile_section = driver.find_elements_by_xpath("//aside")
+    time.sleep(5)
+
+    result = profile_section[0].text.split('\n')
+    print(f'result len: {len(result)}')
+    print(result)
+
+    company_text = result[0]
+    job_title_text = result[-5]
+    job_contract_text = result[-4]
+    location_text = result[-3]
+    application_text = result[-1]
+
+    company_data.append(company_text)
+    job_title_data.append(job_title_text)
+    job_contract_data.append(job_contract_text)
+    location_data.append(location_text)
+    application_data.append(application_text)
+
+    """
+    target button types
+    - 입사 지원하기 : Green signal. click -> modal pop up -> '지원하기' click -> (moved to main page(auto))
+    - 지원 완료 : Red signal. button attr 'disabled' status
+    - 지원하러 가기 : Orange. You should go to another site
+    """
+    # In case of '입사 지원하기' which is applicable directly by single click
+    try:
+        if application_text == '입사 지원하기':
+            apply_modal_btn = driver.find_element_by_xpath("//button[contains(text(), '지원하기')]")
+            apply_modal_btn.click()
+            time.sleep(3)
+
+            apply_btn = driver.find_element_by_xpath("//button[text()='지원하기']")
+            apply_btn.click()
+            time.sleep(3)
+
+            appliedToday_data.append([company_text, job_title_text])
+            print('🔖 Your job application was send successfully!')
+    except:
+        print('입사 지원 불가')
+
+    print('---------------------')
+    time.sleep(5)
+
+print('✅ Scraping tasks are completed')
+
+print(len(company_data))
+print(len(job_title_data))
+print(len(job_contract_data))
+print(len(location_data))
+print(len(application_data))
+
+print('---------------------')
+
+company_data = []
+job_title_data = []
+job_contract_data = []
+location_data = []
+application_data = []
+appliedToday_data = []
+
+df_jobPosts = pd.DataFrame({
+    'company': company_data,
+    'job_title': job_title_data,
+    'job_contract': job_contract_data,
+    'location': location_data,
+    'application type': application_data,
+    'appliedToday': appliedToday_data
+})
+df_jobPosts.to_csv('data.csv', index=False)
+print(df_jobPosts)
